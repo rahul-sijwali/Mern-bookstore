54f2c3ce153bf1045658875235fb5c93eb49488f
Rahul <rahulsijwali@gmail.com>
2024-01-12 07:40:13 +0530

    first

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://root:pass@cluster0.xw0xttt.mongodb.net/?retryWrites=true&w=majority'
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
